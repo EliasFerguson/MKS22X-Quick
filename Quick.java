aeef36dadd1bf7c55681a9377b4ede8b89e8b3f8
Elias Ferguson <eferguson00@stuy.edu>
2019-03-13 11:27:48 -0400

    partition now works I believe, will test further after writing and checking quickSort

--- Quick.java
+++ Quick.java
@@ -1,24 +1,34 @@
 import java.util.*;
 public class Quick {
   public static void main(String[] args) {
-    int[] data = {17, 61, 67, 47, 93,12,	20,	4, 44,	68};
+    int[] data = {17, 61, 67, 47, 93, 12,	20,	4, 44,	68};
     System.out.println(partition(data, 1, 5));
-    for (int i:data) System.out.println(i);
+    for (int i:data) System.out.print(i + " ");
   }
   public static int partition(int[] data, int start, int end) {
     int pivI = start + (int)(Math.random() * (end - start + 1));
     System.out.println(pivI);
+    System.out.println(data[pivI]);
     swap(data, start, pivI);
-    while (start != end) {
+    int i = start + 1;
+    int i2 = end;
+    while (i != i2) {
       if (data[pivI] > data[start]) {
-        start++;
+        i += 1;
       }
-      else {
-        swap(data, pivI, start);
-        end--;
+      else if (data[i] >= data[pivI]){
+        swap(data, i, i2);
+        i2 -= 1;
       }
     }
-    return pivI;
+    if (data[i] < data[pivI]) {
+      swap(data, start, i);
+      return i;
+    }
+    else {
+      swap(data, start, i - 1);
+      return i - 1;
+    }
   }
   public static void swap(int[] data, int a, int b) {
     int temp = data[a];
