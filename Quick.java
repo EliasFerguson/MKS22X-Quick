2610e088a07e7253a95748d5c701463650d6f48b
Elias Ferguson <eferguson00@stuy.edu>
2019-03-14 21:30:52 -0400

    added helper wrapper method to only take one input

--- Quick.java
+++ Quick.java
@@ -3,14 +3,14 @@ public class Quick {
   public static void main(String[] args) {
     int[] data = {17, 61, 67, 47, 93, 12,	20,	4, 44,	68};
     int[] data2 =  {0,2,5,10,15,23};
-    System.out.println(quickSelect(data2, 0));// would return 0
-    System.out.println(quickSelect(data2, 1));//  would return 2
-    System.out.println(quickSelect(data2, 2));//  would return 5
-    System.out.println(quickSelect(data2, 3));//  would return 10
-    System.out.println(quickSelect(data2, 4)); // would return 15
-    System.out.println(quickSelect(data2, 5));//  would return 23
-    //quickSort(data, 0, 9);
-    //for (int i:data) System.out.print(i + " ");
+    //System.out.println(quickSelect(data2, 0));// would return 0
+    //System.out.println(quickSelect(data2, 1));//  would return 2
+  //  System.out.println(quickSelect(data2, 2));//  would return 5
+  ///  System.out.println(quickSelect(data2, 3));//  would return 10
+  //  System.out.println(quickSelect(data2, 4)); // would return 15
+  //  System.out.println(quickSelect(data2, 5));//  would return 23
+    quicksort(data);
+    for (int i:data) System.out.print(i + " ");
   }
   public static int partition(int[] data, int start, int end) {
     int pivI = start + (int)(Math.random() * (end - start + 1));
@@ -43,11 +43,14 @@ public class Quick {
     data[a] = data[b];
     data[b] = temp;
   }
-  public static void quickSort(int[] data, int lo, int hi) {
+  public static void quicksort(int[] data) {
+    quicksort(data, 0, data.length - 1);
+  }
+  public static void quicksort(int[] data, int lo, int hi) {
     if (hi <= lo) return;
     int piv = partition(data, lo, hi);
-    quickSort(data, lo, piv - 1);
-    quickSort(data, piv + 1, hi);
+    quicksort(data, lo, piv - 1);
+    quicksort(data, piv + 1, hi);
   }
   public static int quickSelect(int[] data, int k) {
     int i = 0;
