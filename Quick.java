b7736162a85ef804e4ea66bb560fbecb439c68ee
Elias Ferguson <eferguson00@stuy.edu>
2019-03-14 20:36:19 -0400

    fixed index out of bounds in quickSelect, works now

--- Quick.java
+++ Quick.java
@@ -4,19 +4,19 @@ public class Quick {
     int[] data = {17, 61, 67, 47, 93, 12,	20,	4, 44,	68};
     int[] data2 =  {0,2,5,10,15,23};
     System.out.println(quickSelect(data2, 0));// would return 0
-    System.out.println(quickSelect(data2, 0));//  would return 2
-    System.out.println(quickSelect(data2, 0));//  would return 5
-    System.out.println(quickSelect(data2, 0));//  would return 10
-    System.out.println(quickSelect(data2, 0)); // would return 15
-    System.out.println(quickSelect(data2, 0));//  would return 23
+    System.out.println(quickSelect(data2, 1));//  would return 2
+    System.out.println(quickSelect(data2, 2));//  would return 5
+    System.out.println(quickSelect(data2, 3));//  would return 10
+    System.out.println(quickSelect(data2, 4)); // would return 15
+    System.out.println(quickSelect(data2, 5));//  would return 23
     //quickSort(data, 0, 9);
     //for (int i:data) System.out.print(i + " ");
   }
   public static int partition(int[] data, int start, int end) {
     int pivI = start + (int)(Math.random() * (end - start + 1));
     int piv = data[pivI];
-    System.out.println(pivI);
-    System.out.println(data[pivI]);
+    //System.out.println(pivI);
+    //System.out.println(data[pivI]);
     swap(data, start, pivI);
     int i = start + 1;
     int i2 = end;
@@ -51,14 +51,12 @@ public class Quick {
   }
   public static int quickSelect(int[] data, int k) {
     int i = 0;
-    int i2 = data.length;
-    int pivI = partition(data, 0, data.length) ;
-    if (pivI == k) return data[pivI];
-    while (pivI != i) {
-      pivI = partition(data, i, i2);
+    int i2 = data.length - 1;
+    int pivI = partition(data, i, i2) ;
+    while (pivI != k) {
       if (pivI == k) return data[pivI];
-      if (pivI > k) i2 = pivI - 1;
-      if (pivI < k) i =  pivI + 1;
+      if (pivI > k) pivI = partition(data, i, pivI);
+      if (pivI < k) pivI = partition(data, pivI, i2);;
     }
     return data[pivI];
   }
